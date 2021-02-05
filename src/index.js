93cc951ca54ddc33ffcaf2e36b966c41015ec30d
Wilmer <wcarranzam91@gmail.com>
2021-02-05 09:39:48 -0600

    Renamed style/ to css/

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import "./style/app.scss";
+import "./css/app.scss";
 import App from "./App";
 
 ReactDOM.render(<App />, document.getElementById("app"));
