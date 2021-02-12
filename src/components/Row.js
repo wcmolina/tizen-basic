bc4ae5c3459efed3e0d2806a1b342904cd38517b
Wilmer <wcarranzam91@gmail.com>
2021-02-11 20:45:35 -0600

    Move onMove callback inside Row and RowList

--- src/components/Row.js
+++ src/components/Row.js
@@ -3,28 +3,35 @@ import NavigationContext from "../context/NavigationContext";
 import Card from "./Card";
 import withNavigation from "../hoc/withNavigation";
 
-const onMove = (event) => {
-  console.log("Cool, I'll move to the next/prev card");
+// Stateless component containing row's content
+const RowContent = ({ id, title, movies, className }) => (
+  <>
+    <h2>{title}</h2>
+    <div className={className}>
+      <NavigationContext.Provider value={{ parent: id }}>
+        {movies.map((movie) => (
+          <Card
+            id={`${id}-card-${movie.id}`}
+            key={movie.id}
+            imgUrl={`https://image.tmdb.org/t/p/w342${movie.poster_path}`}
+          />
+        ))}
+      </NavigationContext.Provider>
+    </div>
+  </>
+);
+
+const Row = (props) => {
+  const onMove = (event) => {
+    console.log("Cool, I'll move to the next/prev card");
+  };
+
+  const Navigable = withNavigation({
+    orientation: "horizontal",
+    onMove,
+  })(RowContent);
+
+  return <Navigable {...props} />;
 };
 
-const Row = ({ id, title, movies, className }) => {
-  return movies.length > 0 ? (
-    <>
-      <h2>{title}</h2>
-      <div className={className}>
-        <NavigationContext.Provider value={{ parent: id }}>
-          {movies.map((movie) => (
-            <Card
-              id={`${id}-card-${movie.id}`}
-              key={movie.id}
-              imgUrl={`https://image.tmdb.org/t/p/w342${movie.poster_path}`}
-            />
-          ))}
-        </NavigationContext.Provider>
-      </div>
-    </>
-  ) : // Todo: handle this in the parent component?
-  null;
-};
-
-export default withNavigation({ orientation: "horizontal", onMove })(Row);
+export default Row;
--- src/components/RowList.js
+++ src/components/RowList.js
@@ -3,26 +3,34 @@ import NavigationContext from "../context/NavigationContext";
 import withNavigation from "../hoc/withNavigation";
 import { getComputedHeight } from "../utils/calc";
 
-const onMove = (event = {}) => {
-  try {
-    // Given only cards are focusable and they are in rows, we need to calc the card's row/parent height
-    const { offset = 1, leave = {} } = event;
-    const parentEl = document.getElementById(leave.parent);
-    const height = getComputedHeight(parentEl);
-    const direction = offset === 1 ? "up" : "down";
-    console.log(`I need to move ${height} pixels ${direction}`);
-  } catch (error) {
-    console.log("An error occurred trying to move between rows");
-    console.log(error);
-  }
+// Stateless component containing row list content
+const RowListContent = ({ id, children }) => (
+  <NavigationContext.Provider value={{ parent: id }}>
+    {children}
+  </NavigationContext.Provider>
+);
+
+const RowList = (props) => {
+  const onMove = (event = {}) => {
+    try {
+      // Given only cards are focusable and they are in rows, we need to calc the card's row/parent height
+      const { offset = 1, leave = {} } = event;
+      const parentEl = document.getElementById(leave.parent);
+      const height = getComputedHeight(parentEl);
+      const direction = offset === 1 ? "up" : "down";
+      console.log(`I need to move ${height} pixels ${direction}`);
+    } catch (error) {
+      console.log("An error occurred trying to move between rows");
+      console.log(error);
+    }
+  };
+
+  const Navigable = withNavigation({
+    orientation: "vertical",
+    onMove,
+  })(RowListContent);
+
+  return <Navigable {...props} />;
 };
 
-const RowList = ({ id, children }) => {
-  return (
-    <NavigationContext.Provider value={{ parent: id }}>
-      {children}
-    </NavigationContext.Provider>
-  );
-};
-
-export default withNavigation({ orientation: "vertical", onMove })(RowList);
+export default RowList;
